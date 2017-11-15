2e6084203589dd63fdf20a04b25776ddc3bc7bcd
Gary Song <songary@me.com>
2017-11-15 15:49:53 +0800

    Initial Commit

---
+++ getgit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getgit
+//
+//  Created by Kefan Song on 11/15/17.
+//  Copyright © 2017 Kefan Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
